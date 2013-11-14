5e105c9317a94a6f66bcde595b69034f8d1d222a
nishacodes <nishabatra27@gmail.com>
2013-11-14 17:25:32 -0500

    fix glitch in decoder

--- decoder.rb
+++ decoder.rb
@@ -2,7 +2,7 @@
 
 puts "What is the encoded message? I'll decode it for you"
 stage2 = gets.chomp
-stage1 = stage2.to_s(27).tr("0-9a-q","A-Z")
+stage1 = stage2.to_i.to_s(27).tr("0-9a-q","A-Z")
 secret = stage1.reverse
 puts "The secret is '#{secret}'"
 
