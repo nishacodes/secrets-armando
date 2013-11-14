93df0bbaed92d32beeb09748db64039680a3aa5c
Armando Amador <arm.amador@gmail.com>
2013-11-14 15:31:50 -0500

    Added new string manipulation to decoder and encoder

--- decoder.rb
+++ decoder.rb
@@ -1,7 +1,10 @@
 puts "Enter message to be decoded"
 secret = gets.chomp
 
-#unreverse string
-stage1 = secret.reverse
+#Turn numbers into characters
+stage1 = secret.to_i.to_s(27).tr("0-9a-q", "A-Z")
 
-puts stage1
+#Unreverse characters
+stage2 = stage1.reverse
+
+puts stage2
--- encoder.rb
+++ encoder.rb
@@ -4,4 +4,7 @@ secret = gets.chomp
 #Reverse String
 stage1 = secret.reverse
 
-puts stage1
+#Convert String to numbers
+stage2 = stage1.tr("A-Z", "0-9a-q").to_i(27)
+
+puts stage2
