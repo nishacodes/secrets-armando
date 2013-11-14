43da339afa4b6fa1c777e4bf03be4e735bd71f89
nishacodes <nishabatra27@gmail.com>
2013-11-14 17:12:21 -0500

    add decoder solution

---
+++ decoder.rb
@@ -0,0 +1,8 @@
+# encoder requires a message to be in all CAPS with no spaces"
+
+puts "What is the encoded message? I'll decode it for you"
+stage2 = gets.chomp
+stage1 = stage2.to_s(27).tr("0-9a-q","A-Z")
+secret = stage1.reverse
+puts "The secret is '#{secret}'"
+
