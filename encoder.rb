d8d8085889a81abfc2e1cb59cac295af490a3216
Armando Amador <arm.amador@gmail.com>
2013-11-14 16:27:29 -0500

    Add encoder.rb

---
+++ encoder.rb
@@ -0,0 +1,13 @@
+#Encoder Project
+puts "Enter message to be encoded"
+secret = gets.chomp
+
+#Reverse String
+stage1 = secret.reverse
+
+#Convert String to numbers
+stage2 = stage1.tr("A-Z", "0-9a-q").to_i(27)
+
+puts stage2
+
+
